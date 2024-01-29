acfe1907a107ee4bb966014171ae91e70332e1c2
Paulo Martins <paulo_martins@sapo.pt>
2024-01-29 14:38:18 +0000

    Add EOF

--- src/AcoesAoGerarPedido/CriarPedidoNoBanco.php
+++ src/AcoesAoGerarPedido/CriarPedidoNoBanco.php
@@ -8,6 +8,6 @@ class CriarPedidoNoBanco implements AcaoAposGerarPedido
 {
     public function executaAcao(Pedido $pedido): void
     {
-        echo "Salvando pedido no banco de dados";
+        echo "Salvando pedido no banco de dados". PHP_EOL;
     }
 }
--- src/AcoesAoGerarPedido/LogGerarPedido.php
+++ src/AcoesAoGerarPedido/LogGerarPedido.php
@@ -8,6 +8,6 @@ class LogGerarPedido implements AcaoAposGerarPedido
 {
     public function executaAcao(Pedido $pedido): void
     {
-        echo "Gerando log de geração de pedido";
+        echo "Gerando log de geração de pedido". PHP_EOL;
     }
 }
