f24c2e8953b7b0fe69631e4366f79c62b21c2d31
Paulo Martins <paulo_martins@sapo.pt>
2024-02-20 17:02:08 +0000

    Builder

---
+++ src/NotaFiscal/NotaFiscal.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Alura\DesignPattern\NotaFiscal;
+
+use Alura\DesignPattern\ItemOrcamento;
+
+class NotaFiscal
+{
+    public string $cnpjEmpresa;
+    public string $razaoSocialEmpresa;
+    public array $itens;
+    public string $observacoes;
+    public \DateTimeInterface $dataEmissao;
+    public float $valorImpostos;
+
+    public function valor(): float
+    {
+        return array_reduce(
+            $this->itens,
+            function (float $valorAcumulado, ItemOrcamento $itemAtual) {
+                return $valorAcumulado + $itemAtual->valor;
+            },
+            0
+        );
+    }
+}
